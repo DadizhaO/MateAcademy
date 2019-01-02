13e9790bea4411647d02ea34cd2d0a2af1c6e989
DadizhaO <oleksandr.dadizha@gmail.com>
2019-01-02 17:19:10 +0200

    forgot to add one class

---
+++ src/main/java/HM04/BasicAbstClass.java
@@ -0,0 +1,14 @@
+package HM04;
+
+public abstract class BasicAbstClass {
+
+    private int quantity;
+
+    public BasicAbstClass(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+}
