198fbb6ba449a1f6f85c18d01d248624c3a4b9bf
DadizhaO <oleksandr.dadizha@gmail.com>
2019-01-21 11:39:19 +0200

    Lesson10

---
+++ src/main/java/Lesson10/FruitType.java
@@ -0,0 +1,22 @@
+package Lesson10;
+
+public enum FruitType {
+    STRAWBERRY("Strawberry"), APPLE("Apple"), PEAR("Pear"), ORANGE("Orange");
+
+
+    FruitType(String name) {
+
+        this.name = name;
+
+    }
+
+
+    private String name;
+
+
+    public String toString() {
+
+        return name;
+
+    }
+}
