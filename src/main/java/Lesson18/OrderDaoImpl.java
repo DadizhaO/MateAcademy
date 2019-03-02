c64f89a17b57fc88545e660125c93501f7a26970
DadizhaO <oleksandr.dadizha@gmail.com>
2019-03-02 18:31:54 +0200

    Hibernate HM

--- src/main/java/Lesson18/OrderDaoImpl.java
+++ src/main/java/Lesson18/OrderDaoImpl.java
@@ -12,6 +12,7 @@ import org.hibernate.service.ServiceRegistry;
 
 import java.io.File;
 import java.math.BigDecimal;
+import java.util.HashSet;
 import java.util.Set;
 
 public class OrderDaoImpl implements OrderDao {
@@ -44,39 +45,101 @@ public class OrderDaoImpl implements OrderDao {
 
     @Override
     public Set<Order> getAllOrders() {
-        return null;
+        Set<Order> orders = new HashSet<>();
+        try {
+            sessionObj = buildSessionFactory().openSession();
+            sessionObj.beginTransaction();
+
+            orders = new HashSet<>(sessionObj.createQuery("FROM Order").getResultList());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            sessionObj.close();
+        }
+        return orders;
     }
 
     @Override
     public Order findOrderById(BigDecimal id) {
         Order order = null;
         try {
+
             sessionObj = buildSessionFactory().openSession();
             sessionObj.beginTransaction();
-
-            order = sessionObj.load(Order.class, id);
+            order = sessionObj.get(Order.class, id);
         } catch (Exception sqlException) {
 
             if (null != sessionObj.getTransaction()) {
                 sqlException.printStackTrace();
             }
             sqlException.printStackTrace();
+        } finally {
+            sessionObj.close();
         }
         return order;
     }
 
     @Override
     public boolean insertOrder(Order order) {
-        return false;
+        boolean result = false;
+        try {
+            sessionObj = buildSessionFactory().openSession();
+            sessionObj.beginTransaction();
+            sessionObj.persist(order);
+            sessionObj.getTransaction().commit();
+            result = true;
+        } catch (Exception e) {
+            if (null != sessionObj.getTransaction()) {
+                LOG.info("rollback");
+                sessionObj.getTransaction().rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            sessionObj.close();
+        }
+        return result;
     }
 
     @Override
     public boolean updateOrder(Order order) {
-        return false;
+        boolean result = false;
+        try {
+            sessionObj = buildSessionFactory().openSession();
+            sessionObj.beginTransaction();
+            sessionObj.update(order);
+            sessionObj.getTransaction().commit();
+            result = true;
+        } catch (Exception e) {
+            if (null != sessionObj.getTransaction()) {
+                LOG.info("rollback");
+                sessionObj.getTransaction().rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            sessionObj.close();
+        }
+        return result;
     }
 
     @Override
     public boolean deleteOrder(Order order) {
-        return false;
+        boolean result = false;
+        try {
+            sessionObj = buildSessionFactory().openSession();
+            sessionObj.beginTransaction();
+            sessionObj.delete(order);
+            sessionObj.getTransaction().commit();
+            result = true;
+        } catch (Exception e) {
+            if (null != sessionObj.getTransaction()) {
+                LOG.info("rollback");
+                sessionObj.getTransaction().rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            sessionObj.close();
+        }
+        return result;
     }
 }
